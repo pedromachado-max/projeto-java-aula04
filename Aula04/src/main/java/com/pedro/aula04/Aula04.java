dccb4f37cd7aa89ca9f9edeb883be9db60f7eac7
Pedro <pedro.machado@duque.g12.br>
2025-08-25 19:40:35 -0300

    Primeiro commit da aplicacao

---
+++ Aula04/src/main/java/com/pedro/aula04/Aula04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pedro.aula04;
+
+/**
+ *
+ * @author 08303
+ */
+public class Aula04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
